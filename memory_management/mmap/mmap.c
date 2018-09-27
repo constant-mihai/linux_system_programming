0d90acde878ca322a0bacd4074c572a0eef85f6d
Mihai Constantin <constant.mihai@googlemail.com>
2018-09-27 18:04:03 +0200

    Comments on mmap.

--- memory_management/mmap/mmap.c
+++ memory_management/mmap/mmap.c
@@ -17,6 +17,30 @@
  *
  */
 
+/**
+ * ******************************************************
+ * As an alternative to standard file I/O, the kernel
+ * provides an interface that allows an application to
+ * map a file into memory, meaning that there is a 
+ * one-to-one correspondence between a memory address 
+ * and a word in the file. The programmer can then access
+ * the file directly through memory, identically to any
+ * other chunk of memory-resident data—it is even possible 
+ * to allow writes to the memory region to transparently 
+ * map back to the file on disk.
+ *
+ *  - or -
+ *
+ *  Perhaps because you want to force the use of a memory 
+ *  mapping over the heap for a specific allocation, or 
+ *  perhaps because you are writing your own memory allocation 
+ *  system, you may want to manually create your own 
+ *  anonymous memory mapping—either way, Linux makes it easy. 
+ *  Recall from Chapter 4 that the system call mmap()
+ *  creates a memory mapping and the system call munmap() 
+ *  destroys a mapping:
+ * ******************************************************
+**/
 int main(int argc, char ** argv) {
     void *p;
     int fd;
